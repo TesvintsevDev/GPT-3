0131d6272739cb1f5869561e87ccabd2948f1f14
Eduard Tesvintsev <tesvintsev@gmail.com>
2022-12-23 22:47:13 +0300

    added header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
 
 const Header = () => {
   return (
-	<div>Header</div>
-  )
-}
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise
+          blessing. Indulgence way everything joy alteration boisterous the attachment. Party
+          we&nbsp;years to&nbsp;order allow asked&nbsp;of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">
+            <a href="https://codex-beta.vercel.app/" target="_blank">
+              Get Started
+            </a>
+          </button>
+        </div>
 
-export default Header
+        <div className="gpt3__header-content__people">
+          <img src={people} />
+          <p>1,600 people requested access a&nbsp;visit in&nbsp;last 24&nbsp;hours</p>
+        </div>
+      </div>
+
+      <div className="gpt3__header-image">
+        <img src={ai} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
